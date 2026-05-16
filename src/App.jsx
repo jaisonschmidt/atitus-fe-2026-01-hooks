baa257e85089b4610e39d44d8ebb7d9c3f098245
Jaison Schmidt <31486070+jaisonschmidt@users.noreply.github.com>
2026-05-16 00:49:10 +0000

    exemplo de uso de useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Hello } from "./components/Hello";
 
 function App() {
+  // useEffect permite controlar/detectar o ciclo de vida de um componente
+  useEffect(()=>{
+    console.log("O componente foi montado");
+  }, []);// [] = array vazio é o onLoad do componente
+
+  // dispara uma funcao quando muda o valor de userState
+  useEffect(()=>{
+    console.log("O estado do usuário mudou para: ", userState);
+  }, [userState]);
   // inicializei um estado com o valor inicial false
   // desconstrução de array
   const [userState, setUserState] = useState(false);
